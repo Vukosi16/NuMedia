9a5e5115a839244e0f14a36e2ac932c41b60f14d
Vukosi Mohlabini <vukosimohlabini16@gmail.com>
2026-02-23 13:17:20 +0200

    Finished Hero-Section

--- NuMedaiZa/src/App.jsx
+++ NuMedaiZa/src/App.jsx
@@ -1,10 +1,21 @@
 import Nav from "./Components/Nav"
+import "./styles/appStyles.css"
 
 function App() {
   return (
     <>
       <Nav />
-      <h1>App</h1>
+      <div className="Hero">
+
+        <div className="heroHeadingContainer">
+          <h1 className="heroHeading">Create an Online Presence</h1>
+        </div>
+
+        <div className="heroImageContainer">
+          <img className="heroImage" src="../src/assets/NuMediaStockPhoto.png" alt="NuMedia Stock Photo" />
+        </div>
+
+      </div>
     </>
   )
 }
